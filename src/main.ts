f9444fb51f7193a09d814203db88f0edf1c4c2ac
Christer <hongweibing3@gmail.com>
2023-10-10 22:30:25 +0800

    first commit

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-createApp(App).use(router).mount("#app");
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/reset.css";
+
+const app = createApp(App);
+
+app.use(Antd).use(router).mount("#app");
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,19 @@
+/*
+ * @Author: Christer hongweibing3@gmail.com
+ * @Date: 2023-10-08 15:46:29
+ * @LastEditors: Christer hongweibing3@gmail.com
+ * @LastEditTime: 2023-10-10 22:25:21
+ * @FilePath: \es-frontend\src\router\index.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import HomeView from "../views/IndexPage.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "home",
     component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
 ];
 
