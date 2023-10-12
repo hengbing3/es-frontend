dd22c68bc3051b0cc0255e49d5e946e7ec97b3d8
Christer <hongweibing3@gmail.com>
2023-10-12 23:52:50 +0800

    列表组件

---
+++ src/components/PictureList.vue
@@ -0,0 +1,13 @@
+<!--
+ * @Author: Christer hongweibing3@gmail.com
+ * @Date: 2023-10-12 23:51:56
+ * @LastEditors: Christer hongweibing3@gmail.com
+ * @LastEditTime: 2023-10-12 23:52:15
+ * @FilePath: \es-frontend\src\components\PictureList.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+  <div>用户列表</div>
+</template>
+<script setup lang="ts"></script>
+<style scoped></style>
--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,20 +1,15 @@
 <!--
  * @Author: Christer hongweibing3@gmail.com
- * @Date: 2023-10-10 23:18:41
+ * @Date: 2023-10-12 23:50:36
  * @LastEditors: Christer hongweibing3@gmail.com
- * @LastEditTime: 2023-10-12 23:24:25
- * @FilePath: \es-frontend\src\components\PostList.vue
+ * @LastEditTime: 2023-10-12 23:50:52
+ * @FilePath: \es-frontend\src\components\UserList.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
-  <div>
-    文章列表
-    <img :src="girl" class="my-girl" />
-  </div>
+  <div>用户列表</div>
 </template>
-<script setup lang="ts">
-import girl from "@/assets/linghua.png";
-</script>
+<script setup lang="ts"></script>
 <style scoped>
 .my-girl {
   width: 200px;
