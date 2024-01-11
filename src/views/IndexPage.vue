3a154bace93ddc2b5c6b97bec25e15b779634148
Christer <hongweibin3@gmail.com>
2024-01-11 22:48:13 +0800

    初始化设置默认类型

--- src/views/IndexPage.vue
+++ src/views/IndexPage.vue
@@ -2,7 +2,7 @@
  * @Author: Christer hongweibin3@gmail.com
  * @Date: 2023-10-08 15:46:29
  * @LastEditors: Christer hongweibin3@gmail.com
- * @LastEditTime: 2023-12-17 17:54:20
+ * @LastEditTime: 2024-01-07 17:56:57
  * @FilePath: \es-frontend\src\views\IndexPage.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -42,7 +42,7 @@ import { message } from "ant-design-vue";
 
 const route = useRoute();
 const router = useRouter();
-const activeKey = route.params.category;
+const activeKey = route.params.category ? route.params.category : "post";
 const postList = ref([]);
 const userList = ref([]);
 const pictureList = ref([]);
@@ -75,6 +75,7 @@ const loadAllData = (params: any) => {
  * @param params 请求参数
  */
 const loadData = (params: any) => {
+  console.log(params);
   const { type } = params;
   if (!type) {
     message.error("请选择分类");
